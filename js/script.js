301d5d2c6821c021c72667429cda941d457564f9
John HC <69842878+Purgatos81@users.noreply.github.com>
2021-01-09 17:30:26 +0100

    2 done
    3 left

--- js/script.js
+++ js/script.js
@@ -1,8 +1,16 @@
 // question 1
 
+var num = 0;
+var boo = true;
+var str = "Hello";
 
 // question 2
 
+var firstName = "John";
+var lastName = "Christiansen";
+var fullName = firstName + " " + lastName;
+
+console.log(fullName);
 
 // question 3
 
